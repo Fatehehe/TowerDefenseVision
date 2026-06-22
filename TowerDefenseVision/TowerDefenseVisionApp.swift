926cf3552cade24327d8c0fe3fe4ee6fbb64df76
Fatehehe <fatakhillah.khaqo@gmail.com>
2026-06-22 14:10:34 +0700

    Initial Commit

---
+++ TowerDefenseVision/TowerDefenseVisionApp.swift
@@ -0,0 +1,33 @@
+//
+//  TowerDefenseVisionApp.swift
+//  TowerDefenseVision
+//
+//  Created by Fatakhillah Khaqo on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TowerDefenseVisionApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
